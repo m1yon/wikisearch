f5ee8cf963163d53f7e76b52dc6e6ae2b77f7cc0
= <mlyon360@gmail.com>
2019-02-18 12:42:36 -0700

    more comments and removed FontAwesome

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,64 +1,74 @@
 var xhr;
 var searching = false;
 
-// Initial search call, acts as debouncer
+// initial search call, acts as debouncer
 const search = (searchboxType) => {
-  // Clear search results
-  document.getElementById("resultsDiv").innerHTML = "";
-
-  // Move mainTitle and search up
-  document.getElementById("mainTitle").style.display = "none";
-  document.getElementById("mainTitleAlt").style.display = "flex";
-  document.getElementById("footer").style.display = "none";
-
-  if(searchboxType == "search")
-    document.getElementById("searchAlt").value = document.getElementById("search").value;
-
+  // if not searching and search isn't null
   if(!searching && document.getElementById(searchboxType).value.length > 0) {
-    var cards = document.getElementById("resultsDiv").childNodes;
-    if(cards.length > 3) {
-      // Animate cards here
-      searching = true;
-      window.setTimeout(getSearch, 300);
-    } else {
-      searching = true;
-      getSearch(searchboxType);
+    // clear search results
+    document.getElementById("resultsDiv").innerHTML = "";
+
+    // hide footer during search
+    document.getElementById("footer").style.display = "none";
+
+    // initial search
+    if(searchboxType == "search") {
+      // move mainTitle and search up
+      document.getElementById("mainTitle").style.display = "none";
+      document.getElementById("mainTitleAlt").style.display = "flex";
+      
+      document.getElementById("searchAlt").value = document.getElementById("search").value;
     }
+
+    // start searching
+    searching = true;
+    getSearch(searchboxType);
   }
 }
 
-// Fetches the JSON file using the given keyword
+// fetches the JSON file using the given keyword
 const getSearch = (searchboxType) => {
+  // searchbox user input
   var keyword = document.getElementById(searchboxType).value;
-  xhr = createCORSRequest("GET", "https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=" + keyword + "&prop=info&srlimit=100&origin=*&inprop=url&utf8=&format=json");
-  xhr.send();
 
+  // generate api call
+  xhr = createCORSRequest("GET", `https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=${keyword}&prop=info&srlimit=100&origin=*&inprop=url&utf8=&format=json`);
+  if(xhr) {
+    xhr.send();
+  } else {
+    console.error("CORS not support by browser.")
+    return;
+  }
+
+
+  // successful call
   xhr.onload = function() {
     var data = JSON.parse(xhr.responseText);
     updateSearch(data);
   }
 
+  // error handler
   xhr.onerror = function() {
     console.log("Error");
   }
 }
 
-// Updates the CSS with the new JSON file
+// updates the CSS with the new JSON file
 const updateSearch = (data) => {
-  console.log(data);
-  // Add new search results
   if(data.query.search.length <= 0) {
+    // no results were found
     noResults();
   } else {
+    // results were found, add new search results
     for(var i = 0; i < data.query.search.length; i++) {
-      var card = document.getElementById("resultCard").cloneNode(true);
+      var card = document.getElementById("resultCard").cloneNode(true); // clone children too (true)
       document.getElementById("resultsDiv").appendChild(card);
+
       var cardAtr = card.childNodes;
-      
       cardAtr[1].innerHTML = data.query.search[i].title;
       cardAtr[1].href = `https://en.wikipedia.org/?curid=${data.query.search[i].pageid}`;
       cardAtr[3].innerHTML = `https://en.wikipedia.org/?curid=${data.query.search[i].pageid}`;
-      cardAtr[5].innerHTML = '"...' + data.query.search[i].snippet + '..."';
+      cardAtr[5].innerHTML = `"...${data.query.search[i].snippet}..."`;
       card.style.display = "flex";
     }
   }
@@ -67,31 +77,29 @@ const updateSearch = (data) => {
   searching = false;
 }
 
-// Allows Cross-Origin requests
+// allows cross-origin requests
 const createCORSRequest = (method, url) => {
   var xhr = new XMLHttpRequest();
+  // check if xhr is an XMLHTTPRequest2 object (Chrome, Firefox, etc.)
   if("withCredentials" in xhr) {
     xhr.open(method, url, true);
+  // check if xhr is an XDomainRequest object (IE)
   } else if(typeof XDomainRequest != "undefined") {
     xhr = new XDomainRequest();
     xhr.open(method, url);
+  // else CORS is not supported by the current browser
   } else {
     xhr = null;
   }
-  xhr.setRequestHeader("Api-User-Agent", "sevonefive-fcc-wiki/1.0");
-  
+
+  // Wikipedia requires an Api-User-Agent header
+  xhr.setRequestHeader("Api-User-Agent", "sevonefive-wiki/1.0");
   return xhr;
 }
 
-// Event handler for results = 0
+// event handler for results = 0
 const noResults = () => {
   document.getElementById("resultsDiv").innerHTML = "<h2 style='color: #f4f4f4;'>No results found :(</h2>";
 }
-
-// Startup Main title animation
-
-// Startup Search box animation
-
-// Startup footer animation
 
 
